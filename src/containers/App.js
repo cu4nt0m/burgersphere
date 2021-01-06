8e1f49e793debf47308ae9012102a349675351d1
cu4nt0m <cu4nt0m@gmail.com>
2021-01-06 07:32:31 +0200

    [AUTH] Added Auth page, routes, and Rest API working but no visual response yet for both sign up and sign in

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Layout from '../hoc/Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './Checkout/Checkout';
+import Auth from './Auth/Auth';
+
 import Orders from './Orders/Orders';
 import { Route, Switch } from 'react-router-dom';
 
@@ -14,6 +16,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component= {Checkout} />
             <Route path="/orders" component= {Orders} />
+            <Route path="/auth" component= {Auth} />
             <Route path="/" component= {BurgerBuilder} />
           </Switch>
         </Layout>
