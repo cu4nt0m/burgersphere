937edd6aaa344c9a58dfe1a5e22daeb7dabe63d4
cu4nt0m <cu4nt0m@gmail.com>
2021-01-10 08:08:56 +0200

    [AUTH] Fixed the user logout problem when refreshing the page & guarded the routes depending on user authenticated or not.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
 import Layout from '../hoc/Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './Checkout/Checkout';
@@ -6,25 +8,55 @@ import Auth from './Auth/Auth';
 import Logout from './Auth/Logout/Logout';
 
 import Orders from './Orders/Orders';
-import { Route, Switch } from 'react-router-dom';
-
+import { Route, Switch, Redirect } from 'react-router-dom';
+import * as actions from '../store/actions/index';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
   render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
+    let routes = (
+      <Switch>
+        <Route path="/auth" component= {Auth} />
+        <Route path="/" exact component= {BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
             <Route path="/checkout" component= {Checkout} />
             <Route path="/orders" component= {Orders} />
             <Route path="/auth" component= {Auth} />
             <Route path="/logout" component= {Logout} />
             <Route path="/" component= {BurgerBuilder} />
-          </Switch>
+        </Switch>);
+    }
+    return (
+      <div>
+        <Layout>
+            {routes}      
         </Layout>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+//cause of connect, if routing didnt work in app, we have to include "withRouter" high order wrapper
+//which is included in react-router-dom to fix the problem. now all fine!
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -166,3 +166,4 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Auth);
+
